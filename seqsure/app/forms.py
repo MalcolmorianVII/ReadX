a515c10bfd210f98792a1fb6439e99be7cc22ee9
Belson Malcolm Kutambe <bdkutambe@gmail.com>
2025-01-09 22:32:19 +0200

    converted StringField to passwordField for passwords

--- seqsure/app/forms.py
+++ seqsure/app/forms.py
@@ -1,16 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField,BooleanField
+from wtforms import PasswordField,StringField,SubmitField,BooleanField
 from wtforms.validators import DataRequired,EqualTo
 
 class LoginForm(FlaskForm):
     email = StringField('Email',validators = [DataRequired()])
-    password = StringField('Password',validators = [DataRequired()])
+    password = PasswordField('Password',validators = [DataRequired()])
     submit = SubmitField('Sign in')
 
 class RegisterForm(FlaskForm):
     job_title = StringField('Job title',validators = [DataRequired()])
     email = StringField('Email',validators = [DataRequired()])
     confirm_email = StringField('Confirm Email',validators = [DataRequired(),EqualTo('email', message='Emails must match')])
-    password = StringField('Password',validators = [DataRequired()])
+    password = PasswordField('Password',validators = [DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Register')
